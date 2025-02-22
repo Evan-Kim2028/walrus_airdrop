b511112e2b77bf6b9a3373c3b739c5313c68c0cb
Evan-Kim2028 <ekcopersonal@gmail.com>
2025-02-22 10:36:25 -0500

    docs: update documentation, add code comments

--- readme.md
+++ readme.md
@@ -1,14 +1,19 @@
 # Walrus Airdrop Processor
 
 ## Setup
+
+First, make a new sentio project. Clone the repository and then login with the command:
+`npx @sentio/cli login --api-key 123`
+
+Install sentio with `yarn add @sentio/sdk`. 
+
+To compile the processor locally, run 
+`yarn sentio build`. Once the processor is ready to upload, use `yarn sentio upload` to upload to the Sentio project. The project name in Sentio needs to match the project name defined in `sentio.yaml`. Currently it's set as `walrus_airdrop`. 
+
 To add the walrus airdrop contract to your sentio project, run the following command:
 `yarn sentio add --chain sui_mainnet 0x98af8b8fde88f3c4bdf0fcedcf9afee7d10f66d480b74fb5a3a2e23dc7f5a564 `
 
 ## Processor
 The `WALAirdropProcessor` is initialized to process objects of type `WALAirdrop` from the specified contract, extracting the static fields of the object which contain the airdrop allocation amounts.
-
-## 
-To run the processor, run the following command:
-`yarn sentio install` and then `yarn sentio upload` to send to the Sentio processor project, 
 
 
--- src/processor.ts
+++ src/processor.ts
@@ -1,22 +1,23 @@
-// need to apply iterator over 0x98af8b8fde88f3c4bdf0fcedcf9afee7d10f66d480b74fb5a3a2e23dc7f5a564::airdrop::WALAirdrop to get the owner and wal token static fields.
-
 import { airdrop } from './types/sui/0x98af8b8fde88f3c4bdf0fcedcf9afee7d10f66d480b74fb5a3a2e23dc7f5a564.js'
 import { SuiObjectTypeProcessor } from "@sentio/sdk/sui"
 
 
 export function initWALAirdropProcessor() {
   SuiObjectTypeProcessor.bind({
-  objectType: airdrop.WALAirdrop.type(),
-  startCheckpoint: 114180816n
+    // Specify the object type to track (WALAirdrop)
+    objectType: airdrop.WALAirdrop.type(),
+    // Set the starting checkpoint for processing
+    startCheckpoint: 114180816n
   })
-  .onTimeInterval(async (self, _, ctx) => {
-    await ctx.eventLogger.emit('WALAirdrop_objects', {
-      id: self.data_decoded.id.id,
-      initial_allocation: Number(self.data_decoded.initial_allocation) / 10**9,
-      allocation_withdrawn: self.data_decoded.allocation_withdrawn,
-      locked_id: self.data_decoded.locked_id
+    .onTimeInterval(async (self, _, ctx) => {
+      // Log the static fields of the WALAirdrop object into Sentio event table `WALAirdrop_objects`
+      await ctx.eventLogger.emit('WALAirdrop_objects', {
+        id: self.data_decoded.id.id,
+        initial_allocation: Number(self.data_decoded.initial_allocation) / 10 ** 9, // WAL Decimal conversion
+        allocation_withdrawn: self.data_decoded.allocation_withdrawn,
+        locked_id: self.data_decoded.locked_id
+      })
     })
-  })
 }
 
 initWALAirdropProcessor()
